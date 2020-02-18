8a7900a8ad618c8a6caeafbc3357aa79f3ae8d4d
DiangoGav <diango.gavidia@sitiouno.com>
2020-02-18 08:22:36 -0400

    Äpp Component and testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div data-test="component-app">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error: ""
+    }
+  }
+
+  handleDecrement = () => {
+    if (this.state.counter !== 0) {
+      this.setState((prevState) => ({counter: prevState.counter - 1}));
+    } else{
+      this.setState(() => ({error: "counter can not go below zero."}));
+    }
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+      error: ""
+    }));
+  }
+
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          {this.state.counter}
+        </h1>
+        <button
+          data-test="increment-button"
+          onClick={this.handleIncrement}
+        >Increment counter
+        </button>
+        <button
+          data-test="decrement-button"
+          onClick={this.handleDecrement}
+        >
+          Decrement Counter
+        </button>
+        <h3 data-test="error-message">{this.state.error}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
